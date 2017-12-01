10b542e88e6d0edb77797a3b3c7c6b9ef1308737
Ryan G Ferris <ryanglengerris@gmail.com>
2017-12-01 13:22:39 -0800

    update script.js backend logic for each language

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,47 @@
 $(document).ready(function() {
   // Back end logic
   function cSharpLanguage(typeOfBusiness, companyToWorkFor, projectTypes, typeOfDevelopment, webOrMobileDevelopment, userSoftwareInterest) {
-    if (
-      ((typeOfBusiness = "bigAndEstablished") || ((typeOfBusiness = "enterprise") || ((typeOfBusiness = "startup")) &&
-      ((companyToWorkFor = "mac") || ((companyToWorkFor = "microsoft") || ((companyToWorkFor = "governmentAgency") || ((companyToWorkFor = "")) &&
-      ((projectTypes = "internalSoftware") || (projectTypes = "highPerformanceProcessing") || (projectTypes = "userInterface") || (projectTypes = "serverSide") || (projectTypes = "contentManagementSystems") || (projectTypes = "interactiveWebApplications")) &&
-      ((typeOfDevelopment = "frontEnd") || (typeOfDevelopment = "backEnd") || (typeOfDevelopment = "notSure") || (typeOfDevelopment = "") )&&
-      ((webOrMobileDevelopment = "webDevelopment") || (webOrMobileDevelopment = "mobileDevelopment") || (webOrMobileDevelopment = "") || (webOrMobileDevelopment = "")) &&
-      ((userSoftwareInterest = "android") || (userSoftwareInterest = "drupal") || (userSoftwareInterest = "joomla") || (userSoftwareInterest = "wordpress")) {
+    if ((typeOfBusiness = "bigAndEstablished") && (companyToWorkFor = "microsoft") || (projectTypes = "internalSoftware")) {
       return true;
     } else {
-      console.log("Error! Language");
+      console.log("Error! C# Language");
     }
-  }
   function javaLanguage (typeOfBusiness, companyToWorkFor, projectTypes, typeOfDevelopment, webOrMobileDevelopment, userSoftwareInterest) {
-    if
-    ((typeOfBusiness = "bigAndEstablished") || ((typeOfBusiness = "enterprise") || ((typeOfBusiness = "startup")) &&
-    ((companyToWorkFor = "mac") || ((companyToWorkFor = "microsoft") || ((companyToWorkFor = "governmentAgency") || ((companyToWorkFor = "")) &&
-    ((projectTypes = "internalSoftware") || (projectTypes = "highPerformanceProcessing") || (projectTypes = "userInterface") || (projectTypes = "serverSide") || (projectTypes = "contentManagementSystems") || (projectTypes = "interactiveWebApplications")) &&
-    ((typeOfDevelopment = "frontEnd") || (typeOfDevelopment = "backEnd") || (typeOfDevelopment = "notSure") || (typeOfDevelopment = "") )&&
-    ((webOrMobileDevelopment = "webDevelopment") || (webOrMobileDevelopment = "mobileDevelopment") || (webOrMobileDevelopment = "") || (webOrMobileDevelopment = "")) &&
-    ((userSoftwareInterest = "android") || (userSoftwareInterest = "drupal") || (userSoftwareInterest = "joomla") || (userSoftwareInterest = "wordpress")) {
+    if (
+      (typeOfBusiness = "enterprise") && (companyToWorkFor = "mac") || (companyToWorkFor = "microsoft") && (projectTypes = "highPerformanceProcessing") || (projectTypes = "userInterface") || (typeOfDevelopment = "backEnd") || (webOrMobileDevelopment = "mobileDevelopment") && (userSoftwareInterest = "android") {
       return true;
     } else {
-      console.log("Error! Language");
+      console.log("Error! Java Language");
     }
-  }
+  };
   function phpLanguage (typeOfBusiness, companyToWorkFor, projectTypes, typeOfDevelopment, webOrMobileDevelopment, userSoftwareInterest) {
-    if
-    ((typeOfBusiness = "bigAndEstablished") || ((typeOfBusiness = "enterprise") || ((typeOfBusiness = "startup")) &&
-    ((companyToWorkFor = "mac") || ((companyToWorkFor = "microsoft") || ((companyToWorkFor = "governmentAgency") || ((companyToWorkFor = "")) &&
-    ((projectTypes = "internalSoftware") || (projectTypes = "highPerformanceProcessing") || (projectTypes = "userInterface") || (projectTypes = "serverSide") || (projectTypes = "contentManagementSystems") || (projectTypes = "interactiveWebApplications")) &&
-    ((typeOfDevelopment = "frontEnd") || (typeOfDevelopment = "backEnd") || (typeOfDevelopment = "notSure") || (typeOfDevelopment = "") )&&
-    ((webOrMobileDevelopment = "webDevelopment") || (webOrMobileDevelopment = "mobileDevelopment") || (webOrMobileDevelopment = "") || (webOrMobileDevelopment = "")) &&
-    ((userSoftwareInterest = "android") || (userSoftwareInterest = "drupal") || (userSoftwareInterest = "joomla") || (userSoftwareInterest = "wordpress")) {
+    if ((companyToWorkFor = "governmentAgency") || (projectTypes = "serverSide") || (projectTypes = "contentManagementSystems") || (typeOfDevelopment = "notSure") || (webOrMobileDevelopment = "webDevelopment") || (userSoftwareInterest = "drupal") || (userSoftwareInterest = "joomla") || (userSoftwareInterest = "wordpress")) {
       return true;
     } else {
-      console.log("Error! Language");
+      console.log("Error! PHP Language");
     }
   }
   function rubyLanguage (typeOfBusiness, companyToWorkFor, projectTypes, typeOfDevelopment, webOrMobileDevelopment, userSoftwareInterest) {
-    if
-    ((typeOfBusiness = "bigAndEstablished") || ((typeOfBusiness = "enterprise") || ((typeOfBusiness = "startup")) &&
-    ((companyToWorkFor = "mac") || ((companyToWorkFor = "microsoft") || ((companyToWorkFor = "governmentAgency") || ((companyToWorkFor = "")) &&
-    ((projectTypes = "internalSoftware") || (projectTypes = "highPerformanceProcessing") || (projectTypes = "userInterface") || (projectTypes = "serverSide") || (projectTypes = "contentManagementSystems") || (projectTypes = "interactiveWebApplications")) &&
-    ((typeOfDevelopment = "frontEnd") || (typeOfDevelopment = "backEnd") || (typeOfDevelopment = "notSure") || (typeOfDevelopment = "") )&&
-    ((webOrMobileDevelopment = "webDevelopment") || (webOrMobileDevelopment = "mobileDevelopment") || (webOrMobileDevelopment = "") || (webOrMobileDevelopment = "")) &&
-    ((userSoftwareInterest = "android") || (userSoftwareInterest = "drupal") || (userSoftwareInterest = "joomla") || (userSoftwareInterest = "wordpress")) {
-      return true;
-    } else {
-      console.log("Error! Language");
+    if ((typeOfBusiness = "startup") || (companyToWorkFor = "mac") || (companyToWorkFor = "microsoft") || (companyToWorkFor = "governmentAgency") || (projectTypes = "interactiveWebApplications") || (typeOfDevelopment = "frontEnd") || (typeOfDevelopment = "backEnd") || (typeOfDevelopment = "notSure") || (webOrMobileDevelopment = "webDevelopment") || (userSoftwareInterest = "android") || (userSoftwareInterest = "drupal") || (userSoftwareInterest = "joomla") || (userSoftwareInterest = "wordpress")) {
+    return true;
+      } else {
+        console.log("Error! Language");
+      }
     }
-  }
 
   // Front end logic
+  $("form#surveyQuestionnaire").submit(function(event) {
+      var typeOfBusiness = $("#typeOfBusiness").val();
+      var  projectTypes = $("#projectTypes").val();
+      var  typeOfDevelopment = $("#typeOfDevelopment").val();
+      var  webOrMobileDevelopment = $("#webOrMobileDevelopment").val();
+      var  companyToWorkFor = $("#companyToWorkFor").val();
+      var  userSoftwareInterest = $("#userSoftwareInterest").val();
 
+      if (cSharpLanguage(typeOfBusiness, companyToWorkFor, projectTypes, typeOfDevelopment, webOrMobileDevelopment, userSoftwareInterest)) {
+        console.log("cSharpLanguage");
+        $("#formOutput").text("C# Programming Language!");
+      }
+
+    });
 }); //End of '$(document).ready(function())'
